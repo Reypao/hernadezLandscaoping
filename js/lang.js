7447257f7c665675626fd217e18e346a5276eaba
Efrey Guzman <efrey5guzman@gmail.com>
2026-05-17 21:26:22 -0600

    update footer

--- js/lang.js
+++ js/lang.js
@@ -6,9 +6,18 @@ const translations = {
     nav_testimonials: "Testimonials",
     nav_contact: "Contact",
 
-    hero_title: "Beautiful Outdoor,",
-    hero_text: "Every season",
-    hero_button: "More Info",
+    hero_title_1: "Beautiful Outdoor,",
+    hero_title_2: "Every season",
+    hero_subtitle: "More Info",
+
+    about_title: "Welcome to",
+    about_title_1: "Hernandez Landscaping...",
+    about_text_1: "At Hernandez Landscaping, we believe every outdoor space has the potential to become something extraordinary. Our team combines creativity, dedication, and professional craftsmanship to design landscapes that bring beauty, comfort, and value to your home through every season.",
+    about_text_2: "From elegant garden designs to detailed lawn maintenance, we are passionate about creating outdoor environments where families can relax, connect, and enjoy nature. Your vision inspires every project we build.",
+    about_button: "View All Services",
+    about_subtitle_1: "Garden Care",
+    about_subtitle_2: "Lawn Care",
+    about_text_3: "We create and maintain vibrant, healthy gardens filled with color, balance, and natural beauty. Our team care helps your flowers, plants, and landscape thrive throughout the season.",
 
     service_title_1: "Lawn Care",
     service_text_1: "We keep your lawn healthy, vibrant, and beautifully maintained year-round with professional mowing, edging, fertilization, and seasonal care services.",
@@ -33,9 +42,18 @@ const translations = {
     nav_testimonials: "Testimonios",
     nav_contact: "Contacto",
 
-    hero_title: "Hermoso Jardin,",
-    hero_text: "en cada estacion",
-    hero_button: "Mas Informacion",
+    hero_title_1: "Hermoso Jardin,",
+    hero_title_2: "en cada estacion",
+    hero_subtitle: "Mas Informacion",
+
+    about_title: "Bienvenido a",
+    about_title_1: "Hernandez Landscaping...",
+    about_text_1: "En Hernandez Landscaping, creemos que cada espacio exterior tiene el potencial de convertirse en algo extraordinario. Nuestro equipo combina creatividad, dedicación y trabajo profesional para diseñar jardines que aportan belleza, comodidad y valor a su hogar durante cada temporada.",
+    about_text_2: "Desde diseños elegantes de jardines hasta el mantenimiento detallado del césped, nos apasiona crear ambientes exteriores donde las familias puedan relajarse, conectarse y disfrutar de la naturaleza.  Su visión inspira cada proyecto que realizamos.",
+    about_button: "Nuestros Servicios",
+    about_subtitle_1: "Cuidado de Jardines",
+    about_subtitle_2: "Corte de Cesped",
+    about_text_3: "Creamos y mantenemos jardines vibrantes y saludables, llenos de color, equilibrio y belleza natural. Nuestro equipo ayuda a que sus flores, plantas y paisaje prosperen durante toda la temporada.",
 
     service_title_1: "Cuidado del Césped",
     service_text_1: "Mantenemos su césped saludable, verde y perfectamente cuidado durante todo el año con corte profesional, fertilización y mantenimiento estacional.",
